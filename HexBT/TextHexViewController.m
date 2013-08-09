fc0d519d488a69038d8f834c5c36e4e7e2f31a43
Pan Ziyue <random.rrr3@gmail.com>
2013-08-09 15:24:26 +0800

    added else statement

--- HexBT/TextHexViewController.m
+++ HexBT/TextHexViewController.m
@@ -120,6 +120,10 @@
         else if ([binArray[x] isEqualToString:@"1111"]) {
             hexR=[hexR stringByAppendingString:@"F"];
         }
+        else
+        {
+            hexR=@"Invalid input";
+        }
     }
     return hexR;
 }
